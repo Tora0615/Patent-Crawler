5b8e7df73eb39dd9fd2d67bd0b3971f8e3f32cc7
Tora0615 <5438arthur@gmail.com>
2021-10-16 02:05:23 +0800

    feat : Add functions to get relative patent in main one.

---
+++ part2-relative_patent/p_relative_crawler.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 10:23:09 2021
+
+@author: Arthur
+"""
+import xlsxwriter
+import requests
+import time
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
+
+# -- variable init --
+host = "https://patft.uspto.gov"
+MIN = current_running = 1 # PFIZER INC : 1 ~ 6969
+MAX = 1
+fileName = 'Pfizer'+str(MIN)+'-'+str(MAX)+'.xls'
+excel_current = 0
+
+
+
+# -- File init --
+# New Excel
+wb = xlsxwriter.Workbook(fileName, {'constant_memory': True}) #workbook
+ws = wb.add_worksheet() #建立一個sheet1的表
+#設定列寬
+ws.set_column(0,0,10)
+ws.set_column(1,2,100)
+ws.set_column(3,4,10)
+
+
+# 偽造 UserAgent
+ua = UserAgent(use_cache_server=False)
+headers = {
+    'User-Agent': ua.random,
+    "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    "Accept-Encoding": 'gzip, deflate, br',
+    "Accept-Language": 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    "Cache-Control": 'no-cache',
+    "Connection": 'keep-alive',
+    "Host": 'patft.uspto.gov',
+    "Pragma": 'no-cache',
+    "Referer": 'https://patft.uspto.gov/netahtml/PTO/search-bool.html',
+    "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1"
+}
+
+def changeTimeFormate(input):
+    input = input.replace(", " , " ")
+    temp = input.split(" ")
+    if temp[0] == "January" :
+        return temp[2]+"/01/"+temp[1]
+    elif temp[0] == "February" :
+        return temp[2]+"/02/"+temp[1]
+    elif temp[0] == "March" :
+        return temp[2]+"/03/"+temp[1]
+    elif temp[0] == "April" :
+        return temp[2]+"/04/"+temp[1]
+    elif temp[0] == "May" :
+        return temp[2]+"/05/"+temp[1]
+    elif temp[0] == "June" :
+        return temp[2]+"/06/"+temp[1]
+    elif temp[0] == "July" :
+        return temp[2]+"/07/"+temp[1]
+    elif temp[0] == "August" :
+        return temp[2]+"/08/"+temp[1]
+    elif temp[0] == "September" :
+        return temp[2]+"/09/"+temp[1]
+    elif temp[0] == "October" :
+        return temp[2]+"/10/"+temp[1]
+    elif temp[0] == "November" :
+        return temp[2]+"/11/"+temp[1]
+    elif temp[0] == "December" :
+        return temp[2]+"/12/"+temp[1]
+    
+# 會從主要 6000 多筆專利中，每份都去讀取有關的專利
+def getMainPatentByUrl(url):
+    global excel_current
+     # 無窮迴圈對網頁發request，直到成功
+    while True:
+        try :
+            result = requests.get(url, headers=headers)
+            if result.status_code == 200:
+                soup = BeautifulSoup(result.text,'lxml')
+                
+                #-- get Main patent NO --
+                trList = soup.find_all("tr")
+                PATNO = trList[5].find_all("b")[1].string  #TODO 改為偵測文字 
+                
+                # 寫入 PATNO
+                writeFile(excel_current,0,[PATNO])
+                
+                #-- get relative NO and link --
+                centerList = soup.find_all("center")
+                # 取得正確的 center 並存在 i 
+                for i in range(len(centerList)):
+                    if centerList[i].string == "U.S. Patent Documents" : 
+                        # 從正確 center 取得 table
+                        all_tr_list = centerList[i].find_next_siblings("table")[0].find_all("tr")  
+                        
+                        wrote_count = 0
+                        # 取得所有相關專利的編號與url
+                        for j in range(len(all_tr_list)):
+                            tr_all_td_list = all_tr_list[j].find_all("td")
+                            if len(tr_all_td_list) == 0 : # 防最前面空的表格
+                                continue 
+                            try:
+                                relative_No = tr_all_td_list[0].find_all("a")[0].string
+                                relative_url = tr_all_td_list[0].find_all("a")[0].get("href")
+                            except:
+                                continue   # 防後面空的表格
+                            
+                            # 寫入 relative_No
+                            writeFile(excel_current, 1 , [relative_No])
+                            
+                            # call getRelativePatentByUrl and write
+                            if "http" not in relative_url:
+                                writeFile(excel_current, 2 , getRelativePatentByUrl(host + relative_url))
+                            else:
+                                writeFile(excel_current, 6 , [relative_url])
+                                
+                            # print info
+                            wrote_count += 1
+                            
+                            excel_current += 1
+                            print("  |--- relative : "+str(wrote_count)+" - OK ----")
+                
+                break
+            else:
+                raise ValueError("status_code NOT 200")
+        except Exception as e:
+            print(e)
+            headers['User-Agent'] = ua.random
+            print("sleep 15 sec")
+            time.sleep(15)
+    
+    
+def getRelativePatentByUrl(url):
+    # 無窮迴圈對網頁發request，直到成功
+    while True:
+        try :
+            result = requests.get(url, headers=headers)
+            if result.status_code == 200:
+                soup = BeautifulSoup(result.text,'lxml')
+                returnData = parsingPatentInfo(soup)
+                # break
+                return [returnData["CPC"],returnData['IPC'],returnData['FILED'],returnData['PATDATE']]
+            else:
+                raise ValueError("status_code NOT 200")
+        except Exception as e:
+            print(e)
+            headers['User-Agent'] = ua.random
+            print("sleep 15 sec")
+            time.sleep(15)
+    
+    return parsingPatentInfo(soup)
+    
+
+
+
+def parsingPatentInfo(soup):
+    trList = soup.find_all("tr")
+    
+    try:
+        #PATNO = trList[5].find_all("b")[1].string
+        PATDATE = changeTimeFormate(trList[6].find_all("b")[1].string.replace("\n","").strip())
+    except :
+        #PATNO = ''
+        PATDATE = ''
+        pass
+    
+    
+    FILED =''
+    CPC =''
+    IPC =''
+
+    # 動態，改動態搜尋關鍵字
+    tempcurrent_running = 6
+    while True:
+        if tempcurrent_running > 50:
+            break
+        try:
+            if "Filed" in trList[tempcurrent_running].find_all("th")[0].string :
+                break
+            else:
+                tempcurrent_running += 1
+        except:
+            tempcurrent_running += 1
+    try:
+        FILED = changeTimeFormate(trList[tempcurrent_running].find_all("b")[0].string)
+    except:
+        pass
+
+    
+    tempcurrent_running = 10
+    while True:
+        if tempcurrent_running > 50:
+            break
+        try:
+            if "CPC" in trList[tempcurrent_running].find_all("td")[0].string :
+                break
+            else:
+                tempcurrent_running += 1
+        except:
+            tempcurrent_running += 1
+    try:
+        CPC = trList[tempcurrent_running].find_all("td")[1].string.replace("&nbsp"," ")
+        IPC = trList[tempcurrent_running+1].find_all("td")[1].string.replace("&nbsp"," ")
+    except:
+        pass
+    
+    
+    return {
+        #'PATNO' : PATNO,
+        'PATDATE' : PATDATE,
+        'FILED' : FILED,
+        'CPC' : CPC,
+        'IPC' : IPC
+    }
+    
+
+def writeFile(row,col_offset,data_list):
+    for i in range(len(data_list)):
+        try:
+            ws.write_string(row, col_offset+i, data_list[i])
+        except:
+            pass
+
+
+
+
+print("--------------------------------")
+
+while True:
+    
+    url = host + "/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=4&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r="+str(current_running)+"&f=G&l=50&co1=AND&d=PTXT&s1=%22PFIZER+INC%22&OS=%22PFIZER+INC%22"
+    getMainPatentByUrl(url)
+    print("------------" + str(current_running) + "/" + str(MAX) + "OK ------------")
+    current_running += 1
+    if current_running > MAX:  # 6969
+        break
+
+
+wb.close()
+
+
+            
+        
+        
+        
+        
+        
+        
+        
--- part2-relative_patent/test.py
+++ part2-relative_patent/test.py
@@ -6,10 +6,9 @@ Created on Fri Oct 15 21:59:05 2021
 @author: arthur
 """
 
-def returnArr():
-    a = [1,2,3]
-    return a
+a = input("min : ")
+b = input("max : ")
 
-a = returnArr()
+print("Task : "+str(a)+" - "+str(b))
 
 
